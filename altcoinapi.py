6fe64885ffa335938be137936239e44cf1ed6284
Ryan Gordon <ryangordon210@gmail.com>
2016-10-13 16:32:02 +0100

    Calculate avg of low and highBid

--- altcoinapi.py
+++ altcoinapi.py
@@ -33,7 +33,13 @@ def BTC_SDC():
     print(bittrex['result'][134])
     
     
+    # In this section highBid and lowAsk are unicode
+    highBid  =  data['BTC_SDC']['highestBid']
+    lowAsk = data['BTC_SDC']['lowestAsk']
     
+    # In order to sum the values and not all the values for the list we need to convert the vars to floats
+    # Float allows us to add the decimal numbers together with precision
+    avgBid = (float(highBid) + float(lowAsk)) / 2
     
     # We could just print avgBid but to have a string also we will need to convert to str
     print("This is the btc sdc highest bid "+ highBid)
