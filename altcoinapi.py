17b73cb94b95db7cb67beab7b0b310e17a5143ef
Ryan Gordon <ryangordon210@gmail.com>
2016-10-13 15:56:54 +0100

    SDC Route
    typing ‘/sdc’ returns the object for the SDC market on Poloniex.
    Printing variables that will be needed and returning a JSON dump of the
    object itself. This is needed else unicode is returned

--- altcoinapi.py
+++ altcoinapi.py
@@ -8,10 +8,41 @@ def hello_world():
     print(r)
     data = r.json()
     
+    test = [{key} for key in data.keys()]
 
     # print("The response is" +str(data['BTC_SDC']))
     print(data['BTC_SDC'])
     return "Hello World"
+
+@app.route('/sdc')
+def BTC_SDC():
+    r = requests.get('https://poloniex.com/public?command=returnTicker')
+    
+    print(r)
+    data = r.json()
+
+    
+    
+    for key in data.keys():
+        print(key)
+    
+    # print("The response is" +str(data['BTC_SDC']))
+    print(data['BTC_SDC']['highestBid'])
+
+    #Prints the bittrex api data for coin 134 aka sdc
+    print(bittrex['result'][134])
+    
+    
+    
+    
+    # We could just print avgBid but to have a string also we will need to convert to str
+    print("This is the btc sdc highest bid "+ highBid)
+    print("This is the btc sdc lowest bid "+ lowAsk)
+    print("This is the btc sdc avg bid "+ str(avgBid))
+
+
+    
+    return json.dumps(data['BTC_SDC'])
 
 
 app.run(debug=False)
