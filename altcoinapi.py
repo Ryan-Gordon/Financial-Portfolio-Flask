272a67e1e3f66ed788b585a7efd9177ea70e9a25
FlashGordon95 <ryangordon210@gmail.com>
2016-10-18 18:01:18 +0100

    XMR Route
    Added XMR route.

--- altcoinapi.py
+++ altcoinapi.py
@@ -101,4 +101,51 @@ def BTC_ETH():
     return json.dumps(providedJson)
     # end function
 
+@app.route('/xmr')
+def BTC_XMR():
+    # Pull JSON market data from Poloniex
+    r = requests.get('https://poloniex.com/public?command=returnTicker')
+    # Pull JSON market data from Bittrex
+    b = requests.get('https://bittrex.com/api/v1.1/public/getmarketsummaries')
+    
+    #Print value to user and assign to variable
+    print(r)
+    data = r.json()
+    #Print value to user and assign to variable
+    print(b)
+    bittrex = b.json()
+    print(bittrex)
+    for key in data.keys():
+        print(key)
+    
+    print(data['BTC_XMR']['highestBid'])
+
+    #Prints the bittrex api data for coin 193 aka XMR
+    print(bittrex['result'][193])
+    
+    
+    # In this section highBid and lowAsk are unicode
+    poloHighBid  =  data['BTC_XMR']['highestBid']
+    poloLowAsk = data['BTC_XMR']['lowestAsk']
+    
+    # In order to sum the values and not all the values for the list we need to convert the vars to floats
+    # Float allows us to add the decimal numbers together with precision
+    poloLast = float(data['BTC_XMR']['last'])
+    
+    # Get bittrex data for current market
+    bittrexLast = float(bittrex['result'][193]['Last'])
+
+
+    pricesList = [poloLast, bittrexLast]
+    # Calc avg between 3 markets
+    avgPrice = sum(pricesList) / float(len(pricesList))
+
+    marketName = bittrex['result'][193]['MarketName']
+    # Fill JSON with lowAsk highBid price avgBid 
+    providedJson = {"Market Name": marketName, "poloLast": poloLast, "bittrexLast": bittrexLast, "priceObject": pricesList, "poloLow": poloLowAsk,"poloHighBid": poloHighBid}
+
+    # data['BTC_SDC']
+    return json.dumps(providedJson)
+    # end function
+
 app.run()
