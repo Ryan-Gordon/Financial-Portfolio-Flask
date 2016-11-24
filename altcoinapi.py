05acf6ecc7efc48b9600dd4c4b590570f5bbea13
FlashGordon95 <ryangordon210@gmail.com>
2016-11-24 14:05:28 +0000

    Added port definitions
    Will use port 5000 as default or whatever port required by OS if any

--- altcoinapi.py
+++ altcoinapi.py
@@ -364,5 +364,7 @@ def BTC_XMR():
     # data['BTC_SDC']
     return json.dumps(providedJson)
     # end function
-
-app.run(host='0.0.0.0')
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0')
